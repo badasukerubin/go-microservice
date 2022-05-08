fc661daf0f03721fa3455f935ed4e43aa5e27c02
badasukerubin <kelvinvip8@gmail.com>
2022-05-08 20:43:09 +0100

    Building Microservices with Go: 6 JSON Validation

--- handlers/products.go
+++ handlers/products.go
@@ -77,6 +77,12 @@ func (p Products) MiddlewareProductValidation(next http.Handler) http.Handler {
 			return
 		}
 
+		err = prod.Validate()
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error validating product: %s", err), http.StatusBadRequest)
+			return
+		}
+
 		ctx := context.WithValue(r.Context(), KeyProduct{}, *prod)
 		req := r.WithContext(ctx)
 
